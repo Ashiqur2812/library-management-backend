5e2d452a9f4841618e418d9371fe235bcce7ec49
Ashiqur <ashiqur2812@gmail.com>
2025-07-05 15:40:31 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express, { Application, Request, Response } from 'express';
 import { booksRoutes } from './controller/books.controller';
 import { borrowRoutes } from './controller/borrow.controller';
+import cors from 'cors';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin:
+        ['http://localhost:5173', 'http://localhost:4000'],
+    credentials: true
+}));
 
 app.use('/api/books', booksRoutes);
 app.use('/api/borrow', borrowRoutes);
